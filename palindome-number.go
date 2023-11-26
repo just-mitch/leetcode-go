bd4b11abed71d6874ce1839e1748092bbbdd8115
Mitchell Tracy <mitch@twnsqr.com>
2023-11-26 15:21:04 -0500

    # 9

---
+++ palindome-number.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+
+	s := fmt.Sprint(x)
+
+	head, tail := 0, len(s)-1
+	for head < tail {
+		if s[head] != s[tail] {
+			return false
+		}
+		head++
+		tail--
+	}
+
+	return s[head] == s[tail]
+
+}
---
+++ palindome-number_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"false1", args{12211}, false},
+		{"simple", args{12321}, true},
+		{"simple2", args{1221}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.x); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
