dc9c33c2ddd12e4928c991731b3df2a207d43afe
Mitchell Tracy <mitch@twnsqr.com>
2023-11-26 19:50:31 -0500

    regular expression matching

---
+++ regular-expression-matching.go
@@ -0,0 +1,48 @@
+package main
+
+func isMatch(s string, p string) bool {
+	const unknown = byte(0)
+	const noMatch = byte(1)
+	const match = byte(2)
+	cache := make([][]byte, len(s)+1)
+	for i := range cache {
+		cache[i] = make([]byte, len(p)+1)
+		for j := range cache[i] {
+			cache[i][j] = unknown
+		}
+	}
+
+	var dfs func(int, int) bool
+	dfs = func(iS, iP int) bool {
+
+		if val := cache[iS][iP]; val != unknown {
+			return val == match
+		}
+
+		if iS >= len(s) && iP >= len(p) {
+			cache[iS][iP] = match
+			return true
+		} else if iP >= len(p) {
+			cache[iS][iP] = noMatch
+			return false
+		}
+
+		match := iS < len(s) && (s[iS] == p[iP] || p[iP] == '.')
+		asterisk := iP+1 < len(p) && p[iP+1] == '*'
+
+		if asterisk {
+			return dfs(iS, iP+2) || (match && dfs(iS+1, iP))
+		} else if match {
+			return dfs(iS+1, iP+1)
+		} else {
+			cache[iS][iP] = noMatch
+			return false
+		}
+
+	}
+
+	response := dfs(0, 0)
+
+	return response
+
+}
---
+++ regular-expression-matching_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func Test_isMatch(t *testing.T) {
+	type args struct {
+		s string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"0", args{"aa", "aa"}, true},
+		{"1", args{"aa", "a"}, false},
+		{"2", args{"aa", "a*"}, true},
+		{"3", args{"baa", "a*"}, false},
+		{"4", args{"ab", ".*"}, true},
+		{"5", args{"ab", "ac*b"}, true},
+		{"6", args{"ab", ".*c"}, false},
+		{"7", args{"aaa", "a*a"}, true},
+		{"8", args{"mississippi", "mis*is*ip*."}, true},
+		{"9", args{"aaa", "ab*a*c*a"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
+				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
